9400a86beba6adc75767987a52dd0622c7c1bf1e
aditidtdt <aditidtdt@gmail.com>
2025-06-16 13:57:44 +0530

    added game play for five rounds

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-// player's move
-let playerMove;
-function getHumanChoice() {
-  playerMove = prompt("Enter your move");
-  return playerMove;
-}
-
-// computer's move
-let random = Math.random();
-let move;
-
-function getComputerChoice(random) {
-  if (random < 1/3) {
-    move = 'rock';
-  }
-  else if (random >= 1/3 && random < 2/3) {
-    move = 'paper';
-  }
-  else if (random >= 2/3) {
-    move = 'scissors';
-  }
-  return move;
-}
-
-
-// one round
-
 let humanScore = 0;
 let computerScore = 0;
-const humanMove = getHumanChoice().toLowerCase();
-const computerMove = getComputerChoice(random).toLowerCase();
 
-console.log('You picked: ' + humanMove);
-console.log('The computer picked: ' + computerMove);
 
-let result;
-function playRound(getHumanChoice, computerChoice) {
+function playRound() {
+  // player's move
+  let playerMove;
+  function getHumanChoice() {
+    playerMove = prompt("Enter your move");
+    return playerMove;
+  }
+
+  // computer's move
+  let random = Math.random();
+  let move;
+  function getComputerChoice(random) {
+    if (random < 1/3) {
+      move = 'rock';
+    }
+    else if (random >= 1/3 && random < 2/3) {
+      move = 'paper';
+    }
+    else if (random >= 2/3) {
+      move = 'scissors';
+    }
+    return move;
+  }
+
+  const humanMove = getHumanChoice().toLowerCase();
+  const computerMove = getComputerChoice(random).toLowerCase();
+  console.log('You picked: ' + humanMove);
+  console.log('The computer picked: ' + computerMove);
+
+  let result;
   if (humanMove === computerMove){
     result = 'a tie';
     humanScore++;
@@ -51,5 +48,28 @@ function playRound(getHumanChoice, computerChoice) {
   return result;
 }
 
-// game
+function playGame() {
+  for(let i = 1; i <= 5; i++){
+    console.log(`Round ${i}`);
+    console.log(`RESULT: ${playRound()}`);
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer's score: ${computerScore}`);
+    console.log('\n');
+  }
+  console.log('\n');
+}
+
+playGame();
+if(humanScore > computerScore){
+  console.log('Congratulations, You win!');
+}
+else if (humanScore === computerScore){
+  console.log('Its a tie!');
+}
+else {
+  console.log('NOOO you lost');
+}
+
+
+
 
